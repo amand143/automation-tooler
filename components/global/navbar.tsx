7efd1a3172b49ae1478906e16aa4b67a069eb7eb
amand143 <amanand339@gmail.com>
2024-06-21 23:29:00 +0530

    safari

--- components/global/navbar.tsx
+++ components/global/navbar.tsx
@@ -3,8 +3,10 @@ import React from 'react'
 import { MenuIcon } from 'lucide-react'
 import { UserButton } from '@clerk/nextjs'
 import { auth, currentUser } from '@clerk/nextjs/server'
+import { userAgentFromString } from 'next/server'
 type Props = {}
 const Navbar = async (props: Props) =>{
+
   const user = process.env.UNIVERSAL_USER
     return <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
     <aside className="flex items-center gap-[2px]">
@@ -39,9 +41,8 @@ const Navbar = async (props: Props) =>{
           href="/connections"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
-          <span className=" hidden md:block absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
 
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          <span className="border border-solid border-purple-200  inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             {user ? 'Dashboard' : 'Get Started'}
           </span>
         </Link>
@@ -55,9 +56,9 @@ const Navbar = async (props: Props) =>{
           href="/connections"
           className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         >
-          <span className=" absolute inset-[-1000%]  bg-purple-200" />
 
-          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+
+          <span className="border border-solid border-purple-200 inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
             {user ? 'Dashboard' : 'Get Started'}
           </span>
         </Link>
