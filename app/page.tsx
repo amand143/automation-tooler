165f120e2a35c009764542bead232967f88e3e40
amand143 <amanand339@gmail.com>
2024-06-21 21:45:33 +0530

    mobile 2

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <Navbar />
      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
-     <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]">
+     <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)] hidden md:block">
         <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
         <ContainerScroll
             titleComponent={
@@ -41,7 +41,38 @@ export default function Home() {
             }
           />
           </div>
+
+            
+
           </div>
+
+          <div className="absolute inset-0  h-full w-full items-center  py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]  md:hidden">
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+        <ContainerScroll
+            titleComponent={
+              <div className="flex items-center flex-col">
+                  <Button
+                  size={'lg'}
+                  className="p-8 mb-8 md:mb-0 text-2xl border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500 "
+                >
+         
+                  <a href='/connections' className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black ">
+                    Start For Free Today
+                  </a>
+             
+                </Button>
+                <h1 className="text-5xl md:text-8xl  bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold">
+                  Automate Your Work With Tooler
+                </h1>
+              </div>
+            }
+          />
+          </div>
+
+            
+
+          </div>
+
       </section>
       <InfiniteMovingCards
         className="md:mt-[32rem] mt-[-100px]"
